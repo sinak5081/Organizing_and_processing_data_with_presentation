e9bf7e9ff6246de13ed5af6774840037d1625a1b
sina karimi <sinak5081@gmail.com>
2025-11-15 06:27:01 -0800

    Basic methods of the numpy library

--- Practice/One-dimensional presentations.py
+++ Practice/One-dimensional presentations.py
@@ -18,3 +18,26 @@ print(e)
 #This method returns an array of length 5 and value 1 and data type float,
 f = np.array([1]*5,dtype=float)
 print(f)
+#______________________________________________________________
+# method empty
+#This method creates an array of length 10 with unspecified values.
+g = np.empty(10)
+print(g)
+#This method creates an array of length 10 with string values.
+h = np.empty(10,str)
+print(h)
+#___________________________________________
+#method arange
+#This method is an array of length 10 and each element is equal to its index number.
+r = np.arange(10)
+print(r)
+#_______________________________________________________
+#method ones
+# this method in an array of length 10 and all of value is 1
+n = np.ones(10)
+print(n)
+#__________________________________________________________
+#method random
+#this method in an array of length 10 and each element is random number
+d = np.random.random(10)
+print(d)
