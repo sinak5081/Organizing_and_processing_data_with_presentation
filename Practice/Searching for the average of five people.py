81bd3641e1d64286f3ff442301b9ed8747b5eb2e
sina karimi <sinak5081@gmail.com>
2025-11-16 04:15:42 -0800

    Finding the largest and smallest average value by searching the array and finding the desired value

---
+++ Practice/Searching for the average of five people.py
@@ -0,0 +1,15 @@
+import numpy as np
+grades = np.zeros(5)
+for i in range(len(grades)):
+    grades[i] = int(input("Enter a grade: "))
+print("arraye is:")
+print(grades)
+min = np.amin(grades,0)
+max = np.amax(grades,0)
+print("min is:",min,"max is :",max)
+item = 20
+x = np.where(grades == item)
+if len (x[0]):
+    print("number item found at:",x[0])
+else:
+    print("number is not found")
